b9da430f128d78468daf19223898932d430ed4f9
Piotr Sukiennik <piotrsukiennik@gmail.com>
2013-10-04 19:40:28 +0200

    log4j

--- src/main/java/pl/edu/pjwstk/App.java
+++ src/main/java/pl/edu/pjwstk/App.java
@@ -1,11 +1,17 @@
 package pl.edu.pjwstk;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.spi.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+
+    public static final Logger logger = Logger.getLogger(App.class);
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
