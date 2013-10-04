58189659415d5c2f75d64d10883e8f4d86d8bf38
Piotr Sukiennik <piotrsukiennik@gmail.com>
2013-10-04 19:44:01 +0200

    init

--- src/main/java/pl/edu/pjwstk/App.java
+++ src/main/java/pl/edu/pjwstk/App.java
@@ -12,8 +12,8 @@ public class App
 
     public static final Logger logger = Logger.getLogger(App.class);
 
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ){
+
         System.out.println( "Hello World!" );
     }
 }
