fe534c5d6022820b54ec11fd55b2196bbd5f2210
czarnolecki <s7684@pjwstk.edu.pl>
2013-10-07 20:18:07 +0200

    DRY2

--- src/main/java/pl/edu/pjwstk/datastore/util/BeanToMap.java
+++ src/main/java/pl/edu/pjwstk/datastore/util/BeanToMap.java
@@ -4,16 +4,9 @@ import org.apache.commons.beanutils.BeanMap;
 
 import java.util.Map;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Piotr Sukiennik
- * Date: 06.10.13
- * Time: 00:41
- * To change this template use File | Settings | File Templates.
- */
 public class BeanToMap {
-    public static Map<String,Object> toMap(Object bean){
-        Map<String,Object> map = new BeanMap(bean);
+    public static Map<String, Object> toMap(Object bean) {
+        Map<String, Object> map = new BeanMap(bean);
         return map;
     }
 }
--- src/main/java/pl/edu/pjwstk/datastore/util/XMLToMap.java
+++ src/main/java/pl/edu/pjwstk/datastore/util/XMLToMap.java
@@ -11,27 +11,20 @@ import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Piotr Sukiennik
- * Date: 06.10.13
- * Time: 00:41
- * To change this template use File | Settings | File Templates.
- */
 public class XMLToMap {
 
     private static Pattern INTEGER_PATTERN = Pattern.compile("([\\+-]?\\d+)([eE][\\+-]?\\d+)?");
     private static Pattern DOUBLE_PATTERN = Pattern.compile("[-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?");
-    private static Pattern BOOLEAN_PATTERN = Pattern.compile("true|false",Pattern.CASE_INSENSITIVE);
+    private static Pattern BOOLEAN_PATTERN = Pattern.compile("true|false", Pattern.CASE_INSENSITIVE);
 
 
-    public static Map<String,Object> toMap(String filePath){
+    public static Map<String, Object> toMap(String filePath) {
         try {
-            String xml = IOUtils.toString(new URL("file://"+filePath).openStream());
-            xml="<root>"+xml+"</root>";
+            String xml = IOUtils.toString(new URL("file://" + filePath).openStream());
+            xml = "<root>" + xml + "</root>";
             XMLSerializer xmlSerializer = new XMLSerializer();
-            JSONObject object =  JSONObject.fromObject(xmlSerializer.read(xml));
-            Map<String,Object> out = toParsedMap(object);
+            JSONObject object = JSONObject.fromObject(xmlSerializer.read(xml));
+            Map<String, Object> out = toParsedMap(object);
             return out;
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -39,23 +32,23 @@ public class XMLToMap {
     }
 
 
-    private static Map<String,Object> toParsedMap(Map<String,Object> toParsed){
+    private static Map<String, Object> toParsedMap(Map<String, Object> toParsed) {
 
-        Map<String,Object> linkedHashMap = new LinkedHashMap<String, Object>();
-        for (Map.Entry<String,Object> toParseEntry: toParsed.entrySet()){
-            if (toParseEntry.getValue() instanceof String){
+        Map<String, Object> linkedHashMap = new LinkedHashMap<String, Object>();
+        for (Map.Entry<String, Object> toParseEntry : toParsed.entrySet()) {
+            if (toParseEntry.getValue() instanceof String) {
                 String val = (String) toParseEntry.getValue();
-                if (INTEGER_PATTERN.matcher(val).matches()){
-                    linkedHashMap.put(toParseEntry.getKey(),new Long(val));
-                } else if (DOUBLE_PATTERN.matcher(val).matches()){
-                    linkedHashMap.put(toParseEntry.getKey(),new Double(val));
-                } else if (BOOLEAN_PATTERN.matcher(val).matches()){
-                    linkedHashMap.put(toParseEntry.getKey(),new Boolean(val));
+                if (INTEGER_PATTERN.matcher(val).matches()) {
+                    linkedHashMap.put(toParseEntry.getKey(), new Long(val));
+                } else if (DOUBLE_PATTERN.matcher(val).matches()) {
+                    linkedHashMap.put(toParseEntry.getKey(), new Double(val));
+                } else if (BOOLEAN_PATTERN.matcher(val).matches()) {
+                    linkedHashMap.put(toParseEntry.getKey(), new Boolean(val));
                 } else {
-                    linkedHashMap.put(toParseEntry.getKey(),val);
+                    linkedHashMap.put(toParseEntry.getKey(), val);
                 }
             } else if (toParseEntry.getValue() instanceof Map) {
-                linkedHashMap.put(toParseEntry.getKey(),toParsedMap((Map)toParseEntry.getValue()));
+                linkedHashMap.put(toParseEntry.getKey(), toParsedMap((Map) toParseEntry.getValue()));
             }
         }
 
